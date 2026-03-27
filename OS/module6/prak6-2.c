1713e6cb66d78771ed256406d5fd086ad1c0890d
Clayry <claydanajayo@gmail.com>
2026-03-27 22:21:37 +0700

    tidy things up

--- OS/module6/prak6-2.c
+++ OS/module6/prak6-2.c
@@ -1,30 +1,36 @@
-#include <unistd.h>
-#include <stdlib.h>
-#include <stdio.h>
 #include <pthread.h>
+#include <sched.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
 
 #define NITERS 10000000
-
-pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER; [cite: 48]
+void *count(void *arg);
 unsigned int cnt = 0;
-
-void *count(void *arg) {
-    int i;
-    for (i = 0; i < NITERS; i++) {
-        pthread_mutex_lock(&mutex);   // Lock [cite: 50]
-        cnt++;                        // Critical Region [cite: 51]
-        pthread_mutex_unlock(&mutex); // Unlock [cite: 52]
-    }
-    return NULL;
-}
+pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
 
 int main() {
-    pthread_t tid1, tid2;
-    pthread_create(&tid1, NULL, count, NULL);
-    pthread_create(&tid2, NULL, count, NULL);
-    pthread_join(tid1, NULL);
-    pthread_join(tid2, NULL);
+  pthread_t tid1, tid2;
 
-    printf("Final cnt = %u\n", cnt);
-    return 0;
+  pthread_create(&tid1, NULL, count, NULL);
+  pthread_create(&tid2, NULL, count, NULL);
+  pthread_join(tid1, NULL);
+  pthread_join(tid2, NULL);
+  if (cnt != (unsigned)NITERS * 2)
+    printf("Wrong... cnt = %d\n", cnt);
+  else
+    printf("OK...\tcnt=%d\n", cnt);
+  exit(0);
+}
+
+void *count(void *arg) {
+  int i;
+
+  for (i = 0; i < NITERS; i++) {
+    pthread_mutex_lock(&mutex);
+    cnt++;
+    pthread_mutex_unlock(&mutex);
+  }
+
+  return count;
 }
