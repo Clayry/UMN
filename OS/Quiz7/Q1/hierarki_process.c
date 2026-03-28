f8f1ee2a0a62e6fbdbb53633948ad6b881d6f10a
Clayry <claydanajayo@gmail.com>
2026-03-28 18:01:10 +0700

    updating hierarki_process.c

--- OS/Quiz7/Q1/hierarki_process.c
+++ OS/Quiz7/Q1/hierarki_process.c
@@ -5,40 +5,120 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <unistd.h>
+
+void to_uppercase(char *str) {
+  for (int i = 0; str[i]; i++) {
+    str[i] = toupper((unsigned char)str[i]);
+  }
+}
+
 int main() {
-  char input_string[256];
+  char input[256];
 
   printf("Masukkan string: ");
-  fgets(input_string, sizeof(input_string), stdin);
-  input_string[strcspn(input_string, "\n")] = 0;
-
-  printf("Process A: %d %d : %s\n", getpid(), getppid(), input_string);
-
-  pid_t pid_B = fork();
-
-  if (pid_B == 0) {
-    printf("Process B: %d %d : %s\n", getpid(), getppid(), input_string);
-    pid_t pid_D = fork();
-    if (pid_D == 0) {
-      printf("Process B: %d %d : %s\n", getpid(), getppid(), input_string);
-      exit(0);
-    } else if (pid_D > 0) {
-      waitpid(pid_D, NULL, 0);
-      exit(0);
-    }
-  } else if (pid_B > 0) {
-    waitpid(pid_B, NULL, 0);
-
-    pid_t pid_C = fork();
-    if (pid_C == 0) {
-      for (int i = 0; input_string[i]; i++) {
-        input_string[i] = toupper(input_string[i]);
-      }
-      printf("Process C: %d %d : %s\n", getpid(), getppid(), input_string);
-      exit(0);
-    } else if (pid_C > 0) {
-      waitpid(pid_C, NULL, 0);
-    }
+  fflush(stdout);
+  if (fgets(input, sizeof(input), stdin) == NULL) {
+    perror("fgets");
+    exit(EXIT_FAILURE);
   }
+  input[strcspn(input, "\n")] = '\0';
+
+  int pipe_ac[2];
+  int pipe_bd[2];
+
+  if (pipe(pipe_ac) == -1 || pipe(pipe_bd) == -1) {
+    perror("pipe");
+    exit(EXIT_FAILURE);
+  }
+
+  pid_t pid_b, pid_c;
+
+  // Fork process C
+  pid_c = fork();
+  if (pid_c < 0) {
+    perror("fork C");
+    exit(EXIT_FAILURE);
+  }
+
+  if (pid_c == 0) {
+    close(pipe_ac[1]);
+
+    close(pipe_bd[0]);
+    close(pipe_bd[1]);
+
+    char buf;
+    read(pipe_ac[0], &buf, 1);
+    close(pipe_ac[0]);
+
+    char upper_str[256];
+    strncpy(upper_str, input, sizeof(upper_str));
+    to_uppercase(upper_str);
+
+    printf("Process C: %d %d : %s\n", getpid(), getppid(), upper_str);
+    fflush(stdout);
+    exit(EXIT_SUCCESS);
+  }
+
+  // Fork process B
+  pid_b = fork();
+  if (pid_b < 0) {
+    perror("fork B ");
+    exit(EXIT_FAILURE);
+  }
+
+  if (pid_b == 0) {
+    close(pipe_ac[0]);
+    close(pipe_ac[1]);
+
+    close(pipe_bd[0]);
+
+    pid_t pid_d;
+    pid_d = fork();
+    if (pid_d < 0) {
+      perror("fork D ");
+      exit(EXIT_FAILURE);
+    }
+
+    if (pid_d == 0) {
+      // Process D
+      close(pipe_bd[1]);
+
+      char buf;
+      read(pipe_bd[0], &buf, 1);
+      close(pipe_bd[0]);
+
+      printf("Process D: %d %d : %s\n", getpid(), getppid(), input);
+      fflush(stdout);
+      exit(EXIT_FAILURE);
+    }
+    // Process B
+    printf("Process B: %d %d : %s\n", getpid(), getppid(), input);
+    fflush(stdout);
+
+    char signal = 1;
+    write(pipe_bd[1], &signal, 1);
+    close(pipe_bd[1]);
+
+    waitpid(pid_d, NULL, 0);
+
+    exit(EXIT_SUCCESS);
+  }
+
+  close(pipe_ac[0]);
+
+  close(pipe_bd[0]);
+  close(pipe_bd[1]);
+
+  printf("Process A: %d %d : %s\n", getpid(), getppid(), input);
+  fflush(stdout);
+
+  waitpid(pid_b, NULL, 0);
+
+  char signal = 1;
+  write(pipe_ac[1], &signal, 1);
+  close(pipe_ac[1]);
+
+  waitpid(pid_c, NULL, 0);
+
   return 0;
 }
